73373f93c4f0cd80a876c783731af545d65d77bf
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-08-27 05:41:00 -0700

    added logo and image link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 import './App.css';
 
 function App() {
   return (
     <div >
      <Navigation></Navigation>
+     <Logo></Logo>
+     <ImageLinkForm/>
     </div>
   );
 }
