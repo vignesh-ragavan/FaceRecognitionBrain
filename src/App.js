ed2b13a4e01425088a62a1d9418495fddb6c87ff
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-08-26 23:54:56 -0700

    added navigation components and styles with tachyons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Navigation from './components/Navigation/Navigation'
 import './App.css';
 
 function App() {
   return (
     <div >
-     <hi>welocome Face Brain</hi>
+     <Navigation></Navigation>
     </div>
   );
 }
